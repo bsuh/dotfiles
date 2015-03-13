70fdda67b9069e0544fb265e784d381b74881a66
Brian Suh <brian.j.suh@gmail.com>
2015-03-12 17:39:09 -0700

    cider: fix emacs patch complaint when applied twice

--- patch-emacs.sh
+++ patch-emacs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 brew tap railwaycat/emacsmacport
+git -C /usr/local/Library/Taps/railwaycat/homebrew-emacsmacport/Formula/ reset --hard HEAD
 git -C /usr/local/Library/Taps/railwaycat/homebrew-emacsmacport/Formula/ apply ~/.cider/emacs-mac-formula.diff
