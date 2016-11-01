d0afce578739b71b41e44d7175c282e1d5a66b19
Brian Suh <brian.j.suh@gmail.com>
2016-11-01 10:40:32 -0700

    homebrew: update location to patch homebrew formulas

--- patch-emacs.sh
+++ patch-emacs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 brew tap railwaycat/emacsmacport
-git -C /usr/local/Library/Taps/railwaycat/homebrew-emacsmacport/Formula/ reset --hard HEAD
-git -C /usr/local/Library/Taps/railwaycat/homebrew-emacsmacport/Formula/ apply ~/.cider/emacs-mac-formula.diff
+git -C /usr/local/Homebrew/Library/Taps/railwaycat/homebrew-emacsmacport/Formula/ reset --hard HEAD
+git -C /usr/local/Homebrew/Library/Taps/railwaycat/homebrew-emacsmacport/Formula/ apply ~/.cider/emacs-mac-formula.diff
--- patch-formulas.sh
+++ patch-formulas.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git -C /usr/local/Library/Taps/homebrew/homebrew-core/ reset --hard HEAD
-git -C /usr/local/Library/Taps/homebrew/homebrew-core/ apply ~/.cider/formulas.diff
+git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/ reset --hard HEAD
+git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/ apply ~/.cider/formulas.diff
