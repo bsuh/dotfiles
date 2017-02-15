e4d21c839ee5badcbc741bb1918bbefc263b54cf
Brian Suh <brian.j.suh@gmail.com>
2017-02-14 23:24:53 -0800

    fish: remove outdated functions

--- symlinks/dirs/.config/fish/config.fish
+++ symlinks/dirs/.config/fish/config.fish
@@ -25,30 +25,3 @@ set -gx GOPATH $HOME/go
 function nuget
   mono ~/mybins/nuget.exe $argv
 end
-
-function livestreamer
-  command livestreamer -p mpv --http-header "User-Agent=Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A405 Safari/600.1.4" $argv
-end
-
-function twitch
-  livestreamer --hls-segment-threads 8 "http://www.twitch.tv/"$argv[1] $argv[2..-1]
-end
-
-function peerflix
-  command peerflix --mpv --not-on-top $argv
-end
-
-function emetric-update
-  for folder in (find . -name .git)
-    if test -d $folder/svn
-      echo (dirname $folder)
-      if git -C $folder/.. diff-index --quiet HEAD --
-        git -C $folder/.. svn rebase
-      else
-        git -C $folder/.. stash --quiet
-        git -C $folder/.. svn rebase
-        git -C $folder/.. stash pop --quiet
-      end
-    end
-  end
-end
