4485bd4813e91914ce97020fff435ea4a4d61314
Brian Suh <brian.j.suh@gmail.com>
2017-10-08 12:50:45 -0700

    emacs: remove 24-bit color support patch

--- patch-emacs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-brew tap railwaycat/emacsmacport
-git -C /usr/local/Homebrew/Library/Taps/railwaycat/homebrew-emacsmacport/Formula/ reset --hard HEAD
-git -C /usr/local/Homebrew/Library/Taps/railwaycat/homebrew-emacsmacport/Formula/ apply ~/.cider/emacs-mac-formula.diff
