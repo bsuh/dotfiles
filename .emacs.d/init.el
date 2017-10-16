1daefdbc12a2efaa2c5e83037f37242826c1538b
Brian Suh <brian.j.suh@gmail.com>
2017-10-16 10:56:10 -0700

    fix fzf glitchy rendering issue

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -209,6 +209,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fzf/args "-x --margin 1,0 --print-query")
  '(git-commit-fill-column 72)
  '(git-commit-summary-max-length 50)
  '(package-selected-packages
