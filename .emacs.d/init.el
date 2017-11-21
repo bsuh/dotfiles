6daa09f8c731169a17c8202e7581fbb2b1a48f8d
Brian Suh <brian.j.suh@gmail.com>
2017-11-21 10:47:25 -0800

    clean up init file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,19 +6,13 @@
 ;;; Code:
 
 ;; Replace some variables temporarily during startup for faster startup
-(setq gc-cons-threshold-backup gc-cons-threshold)
-(setq file-name-handler-alist-backup file-name-handler-alist)
+(defvar gc-cons-threshold-backup gc-cons-threshold)
+(defvar file-name-handler-alist-backup file-name-handler-alist)
 (setq gc-cons-threshold (* 100 1024 1024))
 (setq file-name-handler-alist nil)
-(run-with-idle-timer 1 nil (lambda ()
+(run-with-idle-timer 5 nil (lambda ()
                              (setq gc-cons-threshold gc-cons-threshold-backup)
                              (setq file-name-handler-alist file-name-handler-alist-backup)))
-
-(eval-when-compile
-  (if (and (= emacs-major-version 24)
-           (or (= emacs-minor-version 4)
-               (= emacs-minor-version 5)))
-      (require 'cl)))
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
@@ -39,10 +33,7 @@
   '(add-to-list 'company-backends 'company-irony))
 
 (eval-after-load 'flycheck
-  '(add-to-list 'flycheck-mode-hook #'flycheck-irony-setup))
-
-;; don't wrap lines
-(setq-default truncate-lines t)
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
 
 ;; show trailing whitespace
 (add-hook 'prog-mode-hook
@@ -50,55 +41,27 @@
             (hs-minor-mode t)
             (setq-local show-trailing-whitespace t)))
 
-;; customize native UI
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(column-number-mode t)
-
-;; neotree
-(global-set-key [f8] 'neotree-toggle)
-
 ;; smex
-(setq ido-everywhere t)
-(setq ido-use-faces nil)
-(ido-mode t)
-(flx-ido-mode t)
 (global-set-key (kbd "M-x") 'smex)
 
 ;; vim
-(setq evil-want-C-u-scroll t)
-(require 'evil)
-(evil-mode 1)
-(require 'evil-surround)
-(require 'dired)
-(global-evil-surround-mode t)
-(define-key evil-ex-map "b " 'helm-mini) ; instant switch buffer shortcut
-(define-key evil-ex-map "bd " 'ido-kill-buffer) ; instant kill buffer shortcut
 (setenv "FZF_DEFAULT_COMMAND" "ag --hidden --ignore .git -g \"\"")
-(define-key evil-ex-map "e " 'fzf-git) ; better open file
-(define-key evil-normal-state-map (kbd "SPC") 'smex) ; quicker M-x access
-(define-key evil-normal-state-map "'" 'ace-jump-mode)
+(eval-after-load 'evil
+  '(progn
+     (define-key evil-ex-map "b " 'helm-mini) ; instant switch buffer shortcut
+     (define-key evil-ex-map "bd " 'ido-kill-buffer) ; instant kill buffer shortcut
+     (define-key evil-ex-map "e " 'fzf-git) ; better open file
+     (define-key evil-normal-state-map (kbd "SPC") 'smex) ; quicker M-x access
+     (define-key evil-normal-state-map "'" 'ace-jump-mode)))
 (add-hook 'git-commit-mode-hook 'evil-normal-state)
 
 ;; auto completion
-(setq company-dabbrev-downcase nil)
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 2)
-(require 'company)
-(global-company-mode t)
-(define-key company-active-map "\C-n" 'company-select-next)
-(define-key company-active-map "\C-p" 'company-select-previous)
-
-;; flycheck
-(require 'flycheck)
-(global-flycheck-mode t)
-
-;; indentation
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
+(eval-after-load 'company
+  '(progn
+     (define-key company-active-map "\C-n" 'company-select-next)
+     (define-key company-active-map "\C-p" 'company-select-previous)))
 
 ;; parenthesis
-(setq show-paren-delay 0)
 (add-hook 'prog-mode-hook 'show-paren-mode)
 
 ;; higlight current symbol
@@ -107,33 +70,15 @@
             (highlight-symbol-mode)
             (highlight-symbol-nav-mode)))
 
-;; C-style languages
-(setq c-basic-offset 2)
-(setq c-default-style "linux")
-
 ;; html
-(setq web-mode-code-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-markup-indent-offset 2)
 (add-to-list 'auto-mode-alist '("\\.cshtml?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (eval-after-load 'flycheck
-  '(put 'html-tidy 'flycheck-modes (append '(web-mode) (get 'html-tidy 'flycheck-modes))))
+  '(flycheck-add-mode 'html-tidy 'web-mode))
 
-;; javascript
-(setq js-indent-level 2)
-(setq js2-mode-show-parse-errors nil)
-(setq js2-mode-show-strict-warnings nil)
-(add-hook 'js-mode-hook
-          (lambda ()
-            (tide-setup)
-            (eldoc-mode)))
-
-;; jsx
+;; javascript && jsx
 (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-(eval-after-load 'flycheck
-  '(flycheck-add-mode 'javascript-eslint 'web-mode))
+(add-to-list 'auto-mode-alist '("\\.js[x]?$" . web-mode))
 (add-hook 'web-mode-hook
           (lambda ()
             (when (equal web-mode-content-type "jsx")
@@ -142,6 +87,8 @@
               (flycheck-mode)
               (flycheck-select-checker 'javascript-eslint)
               )))
+(eval-after-load 'flycheck
+  '(flycheck-add-mode 'javascript-eslint 'web-mode))
 
 ; https://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
 (defun my/use-eslint-from-node-modules ()
@@ -157,10 +104,7 @@
 (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
 ;; css
-(add-hook 'css-mode-hook
-          (lambda ()
-            (rainbow-mode)
-            (skewer-css-mode)))
+(add-hook 'css-mode-hook 'rainbow-mode)
 
 ;; c#
 (eval-after-load 'company
@@ -185,31 +129,8 @@
 (defalias 'magit-file-log 'magit-log-buffer-file)
 
 ;; org
-(setq org-agenda-files '("~/org/todo.org"))
-(setq org-habit-show-habits-only-for-today nil)
-(setq org-log-done 'time)
 (eval-after-load 'org
   '(add-to-list 'org-modules 'org-habit))
-
-;; color theme
-(load-theme 'zenburn t)
-
-;; custom mode line
-(setq sml/line-number-format "%l")
-(setq sml/mode-width 80)
-(setq sml/name-width 42)
-(setq sml/theme (quote automatic))
-(setq custom-safe-themes '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
-(require 'smart-mode-line)
-(sml/setup)
-
-;; markdown
-(setq markdown-command "grip --export -")
-
-;; automatic window size adjust
-(setq golden-ratio-exclude-modes '("neotree-mode"))
-(require 'golden-ratio)
-(golden-ratio-mode t)
 
 ; elixir
 (eval-after-load 'flycheck
@@ -222,15 +143,50 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
+ '(c-default-style "linux")
+ '(column-number-mode t)
+ '(company-dabbrev-downcase nil)
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 2)
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" default)))
+ '(evil-mode t)
+ '(evil-want-C-u-scroll t)
+ '(flx-ido-mode t)
  '(fzf/args "-x --margin 1,0 --print-query")
  '(git-commit-fill-column 72)
  '(git-commit-summary-max-length 50)
+ '(global-company-mode t)
+ '(global-evil-surround-mode t)
+ '(global-flycheck-mode t)
+ '(golden-ratio-mode t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(ido-use-faces nil)
+ '(indent-tabs-mode nil)
+ '(js-indent-level 2)
+ '(markdown-command "grip --export -")
+ '(menu-bar-mode nil)
+ '(org-agenda-files (quote ("~/org/todo.org")))
+ '(org-habit-show-habits-only-for-today nil)
+ '(org-log-done (quote time))
  '(package-selected-packages
    (quote
-    (rg org-journal fzf protobuf-mode yaml-mode alchemist elixir-mode flycheck-credo ace-jump-mode ag company company-irony csharp-mode evil evil-surround flx-ido flycheck flycheck-irony fsharp-mode go-mode golden-ratio helm highlight-symbol irony magit markdown-mode omnisharp org-plus-contrib projectile rainbow-mode smart-mode-line smex tide web-mode zenburn-theme))))
+    (rg org-journal fzf protobuf-mode yaml-mode alchemist elixir-mode flycheck-credo ace-jump-mode ag company company-irony csharp-mode evil evil-surround flx-ido flycheck flycheck-irony fsharp-mode go-mode golden-ratio helm highlight-symbol irony magit markdown-mode omnisharp org-plus-contrib projectile rainbow-mode smex tide web-mode zenburn-theme)))
+ '(show-paren-delay 0)
+ '(show-paren-mode t)
+ '(tab-width 2)
+ '(tool-bar-mode nil)
+ '(truncate-lines t)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Consolas" :foundry "nil" :slant normal :weight normal :height 141 :width normal)))))
