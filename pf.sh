e640778e15901fadf6bd1c9c8fee0b14d2af15c7
Brian Suh <brian.j.suh@gmail.com>
2018-07-27 10:56:00 -0700

    Enable IPv6 on firewall

--- pf.sh
+++ pf.sh
@@ -7,5 +7,8 @@ rdr pass on en4 inet proto tcp from any to any port 80 -> 127.0.0.1 port 8080
 rdr pass on ppp0 inet proto tcp from any to any port 80 -> 127.0.0.1 port 8080
 block in all
 pass in on lo0
+pass in proto {udp,tcp} from any port 67 to any port 68 keep state
+pass in proto icmp6 all keep state
+pass in proto icmp all keep state
 pass out keep state
 ' | sudo pfctl -F all -f -
