069323ba58e51e8401bdc2634041d069b655e1a6
Brian Suh <brian.j.suh@gmail.com>
2019-07-22 11:12:19 -0700

    emacs: move to distinct customization file

---
+++ .emacs.d/custom.el
@@ -0,0 +1,60 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
+ '(c-default-style "linux")
+ '(column-number-mode t)
+ '(company-dabbrev-downcase nil)
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 2)
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("c82d24bfba431e8104219bfd8e90d47f1ad6b80a504a7900cbee002a8f04392f" "54f2d1fcc9bcadedd50398697618f7c34aceb9966a6cbaa99829eb64c0c1f3ca" "d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" default)))
+ '(evil-mode t)
+ '(evil-want-C-u-scroll t)
+ '(flx-ido-mode t)
+ '(flycheck-erlang-include-path
+   (quote
+    ("/Users/bsuh/git/server/erl/" "/Users/bsuh/git/server/erl/wa/include/" "include/" "../include/")))
+ '(fzf/args "-x --margin 1,0 --print-query")
+ '(git-commit-fill-column 72)
+ '(git-commit-summary-max-length 50)
+ '(global-company-mode t)
+ '(global-evil-surround-mode t)
+ '(global-flycheck-mode t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(ido-use-faces nil)
+ '(indent-tabs-mode nil)
+ '(js-indent-level 2)
+ '(markdown-command "grip --export -")
+ '(menu-bar-mode nil)
+ '(org-agenda-files (quote ("~/org/todo.org")))
+ '(org-habit-show-habits-only-for-today nil)
+ '(org-log-done (quote time))
+ '(package-selected-packages
+   (quote
+    (dockerfile-mode groovy-mode erlang ibuffer-vc cmake-mode flycheck-flow rg org-journal fzf protobuf-mode yaml-mode alchemist elixir-mode flycheck-credo ace-jump-mode ag company company-irony csharp-mode evil evil-surround flx-ido flycheck flycheck-irony fsharp-mode go-mode helm highlight-symbol irony magit markdown-mode omnisharp org-plus-contrib projectile rainbow-mode smex tide web-mode zenburn-theme)))
+ '(show-paren-delay 0)
+ '(show-paren-mode t)
+ '(tab-width 2)
+ '(tool-bar-mode nil)
+ '(truncate-lines t)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-enable-auto-closing nil)
+ '(web-mode-enable-auto-expanding nil)
+ '(web-mode-enable-auto-indentation nil)
+ '(web-mode-enable-auto-opening nil)
+ '(web-mode-enable-auto-pairing nil)
+ '(web-mode-enable-auto-quoting nil)
+ '(web-mode-markup-indent-offset 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "nil" :slant normal :weight normal :height 141 :width normal)))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,6 +23,9 @@
     (package-refresh-contents)
     (package-install-selected-packages)))
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;; c++/c/objective-c completion
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
@@ -168,63 +171,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(c-basic-offset 2)
- '(c-default-style "linux")
- '(column-number-mode t)
- '(company-dabbrev-downcase nil)
- '(company-idle-delay 0)
- '(company-minimum-prefix-length 2)
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes
-   (quote
-    ("d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" default)))
- '(evil-mode t)
- '(evil-want-C-u-scroll t)
- '(flx-ido-mode t)
- '(flycheck-erlang-include-path
-   (quote
-    ("/Users/bsuh/git/server/erl/" "/Users/bsuh/git/server/erl/wa/include/" "include/" "../include/")))
- '(fzf/args "-x --margin 1,0 --print-query")
- '(git-commit-fill-column 72)
- '(git-commit-summary-max-length 50)
- '(global-company-mode t)
- '(global-evil-surround-mode t)
- '(global-flycheck-mode t)
- '(ido-everywhere t)
- '(ido-mode (quote both) nil (ido))
- '(ido-use-faces nil)
- '(indent-tabs-mode nil)
- '(js-indent-level 2)
- '(markdown-command "grip --export -")
- '(menu-bar-mode nil)
- '(org-agenda-files (quote ("~/org/todo.org")))
- '(org-habit-show-habits-only-for-today nil)
- '(org-log-done (quote time))
- '(package-selected-packages
-   (quote
-    (dockerfile-mode groovy-mode erlang ibuffer-vc cmake-mode flycheck-flow rg org-journal fzf protobuf-mode yaml-mode alchemist elixir-mode flycheck-credo ace-jump-mode ag company company-irony csharp-mode evil evil-surround flx-ido flycheck flycheck-irony fsharp-mode go-mode helm highlight-symbol irony magit markdown-mode omnisharp org-plus-contrib projectile rainbow-mode smex tide web-mode zenburn-theme)))
- '(show-paren-delay 0)
- '(show-paren-mode t)
- '(tab-width 2)
- '(tool-bar-mode nil)
- '(truncate-lines t)
- '(web-mode-code-indent-offset 2)
- '(web-mode-css-indent-offset 2)
- '(web-mode-enable-auto-closing nil)
- '(web-mode-enable-auto-expanding nil)
- '(web-mode-enable-auto-indentation nil)
- '(web-mode-enable-auto-opening nil)
- '(web-mode-enable-auto-pairing nil)
- '(web-mode-enable-auto-quoting nil)
- '(web-mode-markup-indent-offset 2))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "nil" :slant normal :weight normal :height 141 :width normal)))))
