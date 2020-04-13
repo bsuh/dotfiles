1a631fc12f12b77110a22969dd860e3e82a461e6
Brian Suh <brian.j.suh@gmail.com>
2020-04-12 22:01:13 -0700

    emacs: deft mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -12,13 +12,18 @@
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
-    ("c82d24bfba431e8104219bfd8e90d47f1ad6b80a504a7900cbee002a8f04392f" "54f2d1fcc9bcadedd50398697618f7c34aceb9966a6cbaa99829eb64c0c1f3ca" "d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" default)))
+    ("76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" "7aaee3a00f6eb16836f5b28bdccde9e1079654060d26ce4b8f49b56689c51904" "f3455b91943e9664af7998cc2c458cfc17e674b6443891f519266e5b3c51799d" "0c9f63c9d90d0d135935392873cd016cc1767638de92841a5b277481f1ec1f4a" "cdb4ffdecc682978da78700a461cdc77456c3a6df1c1803ae2dd55c59fa703e3" "a7051d761a713aaf5b893c90eaba27463c791cd75d7257d3a8e66b0c8c346e77" "c82d24bfba431e8104219bfd8e90d47f1ad6b80a504a7900cbee002a8f04392f" "54f2d1fcc9bcadedd50398697618f7c34aceb9966a6cbaa99829eb64c0c1f3ca" "d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" default)))
+ '(deft-directory "/Users/bsuh/org/")
+ '(deft-extensions (quote ("org" "txt" "text" "md" "markdown")))
  '(evil-mode t)
  '(evil-want-C-u-scroll t)
  '(flx-ido-mode t)
  '(flycheck-erlang-include-path
    (quote
-    ("/Users/bsuh/git/server/erl/" "/Users/bsuh/git/server/erl/wa/include/" "include/" "../include/")))
+    ("/Users/bsuh/git/server/erl/" "/Users/bsuh/git/server/erl/wa/include/" "/Users/bsuh/git/server/erl/_build/default/plugins/" "include/" "../include/")))
+ '(flycheck-erlang-library-path
+   (quote
+    ("/Users/bsuh/git/server/erl/_build/default/plugins/proper/")))
  '(fzf/args "-x --margin 1,0 --print-query")
  '(git-commit-fill-column 72)
  '(git-commit-summary-max-length 50)
@@ -37,7 +42,8 @@
  '(org-log-done (quote time))
  '(package-selected-packages
    (quote
-    (dockerfile-mode groovy-mode erlang ibuffer-vc cmake-mode flycheck-flow rg org-journal fzf protobuf-mode yaml-mode alchemist elixir-mode flycheck-credo ace-jump-mode ag company company-irony csharp-mode evil evil-surround flx-ido flycheck flycheck-irony fsharp-mode go-mode helm highlight-symbol irony magit markdown-mode omnisharp org-plus-contrib projectile rainbow-mode smex tide web-mode zenburn-theme)))
+    (deft dockerfile-mode groovy-mode erlang ibuffer-vc cmake-mode flycheck-flow rg org-journal fzf protobuf-mode yaml-mode alchemist elixir-mode flycheck-credo ace-jump-mode ag company company-irony csharp-mode evil evil-surround flx-ido flycheck flycheck-irony fsharp-mode go-mode helm highlight-symbol irony magit markdown-mode omnisharp org-plus-contrib projectile rainbow-mode smex tide web-mode zenburn-theme)))
+ '(select-enable-clipboard t)
  '(show-paren-delay 0)
  '(show-paren-mode t)
  '(tab-width 2)
